61d3eee85857f32ff3a0bc13aea438f42f34b9dd
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-05-11 17:28:26 +0530

    #37-feat:add a login page

--- src/Componet/Auth/Login.jsx
+++ src/Componet/Auth/Login.jsx
@@ -1,55 +1,3 @@
-// import React, { useState } from 'react';
-// import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Import eye icons from react-icons
-// import './login.css';
-
-// const Login = () => {
-//     const [username, setUsername] = useState('');
-//     const [password, setPassword] = useState('');
-//     const [passwordShown, setPasswordShown] = useState(false);
-
-//     const togglePasswordVisibility = () => {
-//         setPasswordShown(!passwordShown);
-//     };
-
-//     const handleLogin = (event) => {
-//         event.preventDefault();
-//         console.log('Login Attempt:', { username, password });
-//     };
-
-//     return (
-//         <div className="login-container">
-//             <div className="login-form">
-//                 <form onSubmit={handleLogin}>
-//                     <div className="form-group">
-//                         <label>Username:</label>
-//                         <input
-//                             type="text"
-//                             value={username}
-//                             onChange={(e) => setUsername(e.target.value)}
-//                             required
-//                         />
-//                     </div>
-//                     <div className="form-group password-group">
-//                         <label>Password:</label>
-//                         <input
-//                             type={passwordShown ? 'text' : 'password'}
-//                             value={password}
-//                             onChange={(e) => setPassword(e.target.value)}
-//                             required
-//                         />
-//                         <button type="button" onClick={togglePasswordVisibility} className="toggle-password">
-//                             {passwordShown ? <FaEyeSlash /> : <FaEye />}
-//                         </button>
-//                     </div>
-//                     <button type="submit" className="login-button">Login</button>
-//                 </form>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Login;
-
 import React, { useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import './login.css';
@@ -66,7 +14,6 @@ const Login = () => {
     const handleLogin = (event) => {
         event.preventDefault();
         console.log('Login Attempt:', { username, password });
-        // Placeholder for login logic
     };
 
     return (
