819a5d8f9edb7625710859e39092a4a9c75f5015
Tabish <mohdtabishkhan001@gmail.com>
2024-05-12 22:28:26 +0530

    added favicon in tab bar and footer

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,18 +1,17 @@
 /** @format */
 
 import React from 'react';
-import { FaGithub, FaLinkedin, FaTwitter, FaStar } from 'react-icons/fa6';
-import './Footer.css';
+import { FaGithub, FaLinkedin, FaStar, FaTwitter } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
-
+import './Footer.css';
+import favicon from "../image/favicon.png"
 function Footer() {
   return (
     <div>
-      {' '}
       <div className="page_2 footer">
         <div className="footer-wrapper">
           <div className="flex">
-            <h3 className="footer-title">CareerZuction</h3>
+            <h3 className="footer-title"><img src={favicon} alt="logo" className='footer-favicon'/>  CareerZunction</h3>
             <p className="footer-pera">
               CareerZunction is a dynamic platform connecting eager students
               with valuable internship opportunities.
@@ -26,17 +25,17 @@ function Footer() {
           <div className="flex docGroup">
             <h4 className="doc">Documentation</h4>
             <div className="doc-pera">
-              <p className="doc-pera doc-perag" onClick={()=>{window.open('https://opensource.com/resources/what-open-source')}}>What is Open-Source.</p>
-              <p className="doc-pera doc-perag" onClick={()=>{window.open('https://opensource.guide/how-to-contribute/')}}>What is Git and GitHub.</p>
-              <p className="doc-pera doc-perag" onClick={()=>{window.open('https://docs.github.com/en/get-started/using-git/about-git')}}>
+              <p className="doc-pera doc-perag" onClick={() => { window.open('https://opensource.com/resources/what-open-source') }}>What is Open-Source.</p>
+              <p className="doc-pera doc-perag" onClick={() => { window.open('https://opensource.guide/how-to-contribute/') }}>What is Git and GitHub.</p>
+              <p className="doc-pera doc-perag" onClick={() => { window.open('https://docs.github.com/en/get-started/using-git/about-git') }}>
                 How to start contributing to Open-Source.
               </p>
             </div>
           </div>
           <div className="flex licenseGroup">
             <h3 className="lice ">License </h3>
-            <h5 className="license underline"  style={{cursor:'pointer'}}>MIT License</h5>
-            <h5 className="coc underline" style={{cursor:'pointer'}}>Code of Conduct</h5>
+            <h5 className="license underline" style={{ cursor: 'pointer' }}>MIT License</h5>
+            <h5 className="coc underline" style={{ cursor: 'pointer' }}>Code of Conduct</h5>
           </div>
 
           <button size="size" type="defautl" className="button-star">
