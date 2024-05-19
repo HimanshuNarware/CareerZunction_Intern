df027244f09338f048bf932d5de4e20b598f2e1d
psychic-coder <rohitganguly450@gmail.com>
2024-05-19 09:20:15 +0530

    improved the hover effect of the icons

--- src/Componet/Footer.jsx
+++ src/Componet/Footer.jsx
@@ -11,103 +11,104 @@ const iconstyle = {
   borderRadius: "50%",
   height: "25px",
   width: "25px",
-  color:"#892BE2"
+  color: "#892BE2",
 };
 function Footer() {
   return (
-
-      <footer className="footer">
-        <div className="containerFooter">
-          <div className="row" id="row">
-            <div
-              className="footer-col"
-              id="logoRow"
-              style={{ paddingTop: "0" }}
+    <footer className="footer">
+      <div className="containerFooter">
+        <div className="row" id="row">
+          <div className="footer-col" id="logoRow" style={{ paddingTop: "0" }}>
+            <h4
+              className="logo"
+              id="logo"
+              style={{ paddingLeft: "0", color: "#892BE2" }}
             >
-              <h4 className="logo" id="logo" style={{ paddingLeft: "0" ,color:"#892BE2"}}>
-                {" "}
-                CareerZunction
-              </h4>
-              <p style={{ color: "#bbbbbb" }}>
-                Career junction is a dynamic platform connecting eager students
-                with valuable internship opportunity.
-              </p>
-            </div>
-            <div className="footer-col">
-              <h4>Documentation</h4>
-              <ul>
-                <li style={{ paddingLeft: "0" }}>
-                  <a
-                    className="doc-pera doc-perag"
-                    onClick={() => {
-                      window.open(
-                        "https://opensource.com/resources/what-open-source"
-                      );
-                    }}
-                  >
-                    What is open-source ?
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="doc-pera doc-perag"
-                    onClick={() => {
-                      window.open(
-                        "https://opensource.guide/how-to-contribute/"
-                      );
-                    }}
-                  >
-                    What is git and github ?
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="doc-pera doc-perag"
-                    onClick={() => {
-                      window.open(
-                        "https://docs.github.com/en/get-started/using-git/about-git"
-                      );
-                    }}
-                  >
-                    How to start contributing to open source ?
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="footer-col">
-              <h4>License</h4>
-              <ul>
-                <li>
-                  <a href="#">MIT License</a>
-                </li>
-                <li>
-                  <a href="#">Code of Conduct</a>
-                </li>
-              </ul>
-            </div>
-            <div className="footer-col">
-              <h4>follow us</h4>
-              <div className="social-links">
-                <a href="https://github.com/HimanshuNarware" target="_blank">
-                  <FaGithub style={iconstyle} />
-                </a>
+              {" "}
+              CareerZunction
+            </h4>
+            <p style={{ color: "#bbbbbb" }}>
+              Career junction is a dynamic platform connecting eager students
+              with valuable internship opportunity.
+            </p>
+          </div>
+          <div className="footer-col">
+            <h4>Documentation</h4>
+            <ul>
+              <li style={{ paddingLeft: "0" }}>
                 <a
-                  href="https://www.linkedin.com/in/HimanshuNarware/"
-                  target="_blank"
+                  className="doc-pera doc-perag"
+                  onClick={() => {
+                    window.open(
+                      "https://opensource.com/resources/what-open-source"
+                    );
+                  }}
                 >
-                  <FaLinkedin style={iconstyle} />
+                  What is open-source ?
                 </a>
-                <a href="https://twitter.com/N_Himanshu_" target="_blank">
-                  <FaTwitter style={iconstyle} />
+              </li>
+              <li>
+                <a
+                  className="doc-pera doc-perag"
+                  onClick={() => {
+                    window.open("https://opensource.guide/how-to-contribute/");
+                  }}
+                >
+                  What is git and github ?
                 </a>
-                <a href="https://github.com/HimanshuNarware/CareerZunction_Intern" target="_blank">
-                  <IoStar style={iconstyle} />
+              </li>
+              <li>
+                <a
+                  className="doc-pera doc-perag"
+                  onClick={() => {
+                    window.open(
+                      "https://docs.github.com/en/get-started/using-git/about-git"
+                    );
+                  }}
+                >
+                  How to start contributing to open source ?
                 </a>
-              </div>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>License</h4>
+            <ul>
+              <li>
+                <a href="#">MIT License</a>
+              </li>
+              <li>
+                <a href="#">Code of Conduct</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>follow us</h4>
+            <div className="social-links">
+              <a href="https://github.com/HimanshuNarware" target="_blank">
+                <FaGithub style={iconstyle} />
+              </a>
+
+              <a
+                href="https://www.linkedin.com/in/HimanshuNarware/"
+                target="_blank"
+              >
+                <FaLinkedin style={iconstyle} />
+              </a>
+              <a href="https://twitter.com/N_Himanshu_" target="_blank">
+                <FaTwitter style={iconstyle} />
+              </a>
+              <a
+                href="https://github.com/HimanshuNarware/CareerZunction_Intern"
+                target="_blank"
+              >
+                <IoStar style={iconstyle} />
+              </a>
             </div>
           </div>
         </div>
-      </footer>
+      </div>
+    </footer>
   );
 }
 
