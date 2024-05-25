81fc58c9a38b15473b6ee52ad4b5165ee3046178
DevanshPaliwal <78657285+DevanshPaliwal@users.noreply.github.com>
2024-05-25 23:29:29 +0530

    Added description on home page

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -44,7 +44,8 @@ function Home() {
             Future</h2>
           <h4>
             <i>Launch your career.<br>Find your perfect internship today.</i><br>
-            <i>Land the internship that gives you a head start.<br> We connect students with exciting opportunities
+            <i>Land the internship that gives you a head start. 
+              We connect students with exciting opportunities
               and guide them through the application process.</i>
             <i>From searching to securing, we're your one-stop shop for landing the perfect internship. 
               Get matched with top companies, build a resume that shines, and learn application secrets from the pros.</i>
