6c47facc5a7578872f3df0abbb89e925fd53e1d4
drick-infinity <harshimalviya47487@gmail.com>
2024-06-02 23:31:32 +0530

    changes are done

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -38,14 +38,17 @@ function Footer() {
             <h3 className='links'>Get in touch</h3>
             <div className="footer-socail-icon">
               <a href="https://github.com/HimanshuNarware" target="_blank">
-              <FaGithub className="github" />
-              </a>
-              <a href="https://www.linkedin.com/in/HimanshuNarware/" target="_blank">
-              <FaLinkedin className="linkedin" />
-              </a>
-              <a href="https://twitter.com/N_Himanshu_" target="_blank">
-              <FaTwitter className="twittor" />
-              </a>
+<FaGithub className="github" />
+</a>
+              
+<a href="https://www.linkedin.com/in/HimanshuNarware/" target="_blank">
+<FaLinkedin className="linkedin" />
+</a>
+<a href="https://twitter.com/N_Himanshu_" target="_blank">
+<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="white" class="bi bi-twitter-x" viewBox="0 0 16 16">
+<path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+</svg>
+</a>
             </div>
           </div>
        <div className='flex qrsec'>
@@ -53,10 +56,10 @@ function Footer() {
           <button size="size" type="defautl" className="button-star">
             <Link
               to="https://github.com/HimanshuNarware/CareerZunction_Intern"
-              target='_blank' className='star-us'>
+              target='_blank' className='star-us' style={{color:'black',textDecoration:'none'}}>
               {' '}
               <FaStar
-                style={{ margin: '-2px 2px', lineHeight: '2px' }}
+                style={{ margin: '-2px 2px', lineHeight: '2px' ,color:' #fcf268'}}
                 className="star"
               />
               Rate us!
@@ -73,3 +76,7 @@ function Footer() {
 }
 
 export default Footer;
+
+
+
+
