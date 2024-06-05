907b670d5c73f9f3c4eabb50f31c28bc7e4bff05
drick-infinity <harshimalviya47487@gmail.com>
2024-06-05 13:47:16 +0530

    updation

--- src/Component/Documetation/Detail.jsx
+++ src/Component/Documetation/Detail.jsx
@@ -31,6 +31,7 @@ function Detail() {
                   
                       <div className="section_founder">
                           <img className="fimg" src={Founder_img} alt="img" />
+                          <div class='content'> 
                           <p>Founder : Himanshu Narware</p>
                           <span>
                           <a  href="https://github.com/HimanshuNarware" target="_blank">
@@ -40,16 +41,17 @@ function Detail() {
               <FaLinkedin className="linkedin" />
               </a>
               <a href="https://twitter.com/N_Himanshu_" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="white" class="bi bi-twitter-x" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
   <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
 </svg>
               </a>
                           </span>
-
+</div>
                       </div>
               
-                      <div className="section_maintainer">
+                      <div className="section_maintainer ">
                           <img className='fimg' src={Maintainer_img} alt="img" />
+                          <div class='content'>
                           <p>Maintainer : Chaitali Sonare</p>
                           <span>
                           <a href="https://github.com/HimanshuNarware" target="_blank">
@@ -59,12 +61,13 @@ function Detail() {
               <FaLinkedin className="linkedin" />
               </a>
               <a href="https://twitter.com/N_Himanshu_" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="white" class="bi bi-twitter-x" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
   <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
 </svg>
               </a>
                               
                           </span>
+                          </div>
                       </div>
                   </div>) :
                   <Slider />}
