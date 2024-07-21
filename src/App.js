e7cce99d25bf0ce1ea0ef9120b471a98aee1ffd1
Naman Jain <141808883+NamanJain795@users.noreply.github.com>
2024-07-21 16:42:17 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -7,32 +7,40 @@ import InternPage from "./Component/Documetation/Internship/InternPage";
 import Footer from "./Component/Footer";
 import Contact from "./Component/Contact";
 import Blog from "./Component/Documetation/Blog";
-import { useState } from "react";
-
+import Login from "./Component/Login";
+import Register from "./Component/Register";
+import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const spinner = document.getElementById("spinner");
-  if (spinner) {
-    setTimeout(() => {
-      spinner.style.display = "none";
-      setLoading(false);
-    }, 2000);
-  }
-  return(
+
+  useEffect(() => {
+    const spinner = document.getElementById("spinner");
+    if (spinner) {
+      setTimeout(() => {
+        spinner.style.display = "none";
+        setLoading(false);
+      }, 2000);
+    }
+  }, []);
+
+  return (
     !loading && (
- <div>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/docs" element={<Doc/>}/>
-        <Route path="/intern" element={<InternPage/>}/>
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/Contact" element={<Contact></Contact>}/>
-        <Route path="*" element={<Error/>}/>
-      </Routes>
-      <Footer/> 
-    </div>)
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/docs" element={<Doc />} />
+          <Route path="/intern" element={<InternPage />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </div>
+    )
   );
 }
 
