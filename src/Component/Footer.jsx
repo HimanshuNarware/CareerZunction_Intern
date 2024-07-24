ad6b199e3a3c5665dc080040c105383968f223d6
Radhika Malpani <radhikavishnudas2002@gmail.com>
2024-07-24 17:13:43 +0530

    Update Footer.jsx

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -43,7 +43,7 @@ function Footer() {
           <ul className="footer-links">
             <li><span> <a href="#">Privacy Policy</a></span></li>
             <li><span><a href="#">Terms and Conditions</a></span></li>
-            <li><span><a href="#">MIT License</a></span></li>
+            <li><span><a href="/licensing">MIT License</a></span></li>
             <li><span><a href="https://github.com/HimanshuNarware/CareerZunction_Intern?tab=coc-ov-file#readme">Code of Conduct</a></span></li>
           </ul>
           <h4 className="footer-heading">Get in touch</h4>
