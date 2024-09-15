6565ffb6b31ba7d618b6f29de68ed1eafa570307
Himanshu Narware <himanshunarware77@gmail.com>
2024-09-15 18:14:17 +0530

    improve api fetching and fix ui issue

--- src/Component/Contributors.jsx
+++ src/Component/Contributors.jsx
@@ -2,53 +2,55 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Contributors.css';
 import Preloader from './Preloader';
+import data from '../DB/Contributor.json'
 
 function Contributors() {
   const [contributors, setContributors] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchContributors() {
-      let allContributors = [];
-      let page = 1;
+  // to improve the fetching speed
+  // useEffect(() => {
+  //   async function fetchContributors() {
+  //     let allContributors = [];
+  //     let page = 1;
 
-      try {
-        while (true) {
-          const response = await axios.get(
-            `https://api.github.com/repos/HimanshuNarware/CareerZunction_Intern/contributors`,
-            {
-              params: {
-                per_page: 100,
-                page,
-              },
-            }
-          );
-          const data = response.data;
-          if (data.length === 0) {
-            break;
-          }
-          allContributors = [...allContributors, ...data];
-          page++;
-        }
-        setContributors(allContributors);
-      } catch (error) {
-        console.error('Error in fetching contributors:', error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchContributors();
-  }, []);
+  //     try {
+  //       while (true) {
+  //         const response = await axios.get(
+  //           `https://api.github.com/repos/HimanshuNarware/CareerZunction_Intern/contributors`,
+  //           {
+  //             params: {
+  //               per_page: 100,
+  //               page,
+  //             },
+  //           }
+  //         );
+  //         const data = response.data;
+  //         if (data.length === 0) {
+  //           break;
+  //         }
+  //         allContributors = [...allContributors, ...data];
+  //         page++;
+  //       }
+  //       setContributors(allContributors);
+  //     } catch (error) {
+  //       console.error('Error in fetching contributors:', error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
+  //   fetchContributors();
+  // }, []);
 
-  if (loading) {
-    return <Preloader />;
-  }
+  // if (loading) {
+  //   return <Preloader />;
+  // }
 
   return (
     <div className="contributors-container">
       <h1 className="contributors-title">Our Contributors</h1>
       <div className="contributors-grid">
-        {contributors.map((contributor) => (
+        {data.map((contributor) => (
           <div key={contributor.id} className="contributor-card">
             <a
               href={contributor.html_url}
